7f609b58558a4bf72b6ba907dd93b60d135e6479
Abu Desai <abhyuday.desai@gmail.com>
2023-09-17 18:06:35 -0700

    remove logic to replace special characters from column names

--- src/prediction/predictor_model.py
+++ src/prediction/predictor_model.py
@@ -74,10 +74,7 @@ class Regressor:
             train_inputs (pandas.DataFrame): The features of the training data.
             train_targets (pandas.Series): The labels of the training data.
         """
-        # lightgbm throws an error if column names contain special characters
-        updated_train_inputs = \
-            train_inputs.rename(columns = lambda x:re.sub('[^A-Za-z0-9_]+', '', x))
-        self.model.fit(updated_train_inputs, train_targets)
+        self.model.fit(train_inputs, train_targets)
         self._is_trained = True
 
     def predict(self, inputs: pd.DataFrame) -> np.ndarray:
