8d878ea2896b0297b0db8442a484d4d08c56fef1
Abu Desai <abhyuday.desai@gmail.com>
2023-09-17 17:38:51 -0700

    first commit

---
+++ src/prediction/predictor_model.py
@@ -0,0 +1,212 @@
+import os
+import re
+import warnings
+from typing import Optional
+
+import joblib
+import numpy as np
+import pandas as pd
+from sklearn.neural_network  import MLPRegressor
+from sklearn.exceptions import NotFittedError
+
+warnings.filterwarnings("ignore")
+
+
+PREDICTOR_FILE_NAME = "predictor.joblib"
+
+
+class Regressor:
+    """A wrapper class for the MLP Regressor.
+
+    This class provides a consistent interface that can be used with other
+    regressor models.
+    """
+
+    model_name = "MLP Regressor"
+
+    def __init__(
+        self,
+        hidden_layer_size: Optional[int] = 100,
+        activation: Optional[str] = "relu",
+        solver: Optional[str] = "adam",
+        learning_rate: Optional[str] = "adaptive",
+        **kwargs,
+    ):
+        """Construct a new MLP Regressor.
+
+        Args:
+            hidden_layer_size (int, optional): Number of neurons in the single
+                hidden layer.
+                Defaults to 100.
+            activation (int, str): Activation function for the hidden layer.
+                Options: ['identity', 'logistic', 'tanh', 'relu']
+                Defaults to "relu".
+            solver (str, optional): The solver for weight optimization.
+                Options: ['lbfgs', 'sgd', 'adam']
+                Defaults to "adam".
+            learning_rate (str, optional): Learning rate schedule for weight updates.
+                Options: ['constant', 'invscaling', 'adaptive']
+                Defaults to "adaptive".
+        """
+        self.hidden_layer_size = int(hidden_layer_size)
+        self.activation = activation
+        self.solver = solver
+        self.learning_rate = learning_rate
+        self.model = self.build_model()
+        self._is_trained = False
+
+    def build_model(self) -> MLPRegressor:
+        """Build a new regressor."""
+        model = MLPRegressor(
+            hidden_layer_sizes=(self.hidden_layer_size,),
+            activation=self.activation,
+            solver=self.solver,
+            learning_rate=self.learning_rate,
+            max_iter=500,
+            random_state=123
+        )
+        return model
+
+    def fit(self, train_inputs: pd.DataFrame, train_targets: pd.Series) -> None:
+        """Fit the regressor to the training data.
+
+        Args:
+            train_inputs (pandas.DataFrame): The features of the training data.
+            train_targets (pandas.Series): The labels of the training data.
+        """
+        # lightgbm throws an error if column names contain special characters
+        updated_train_inputs = \
+            train_inputs.rename(columns = lambda x:re.sub('[^A-Za-z0-9_]+', '', x))
+        self.model.fit(updated_train_inputs, train_targets)
+        self._is_trained = True
+
+    def predict(self, inputs: pd.DataFrame) -> np.ndarray:
+        """Predict regression targets for the given data.
+
+        Args:
+            inputs (pandas.DataFrame): The input data.
+        Returns:
+            numpy.ndarray: The predicted regression targets.
+        """
+        return self.model.predict(inputs)
+
+    def evaluate(self, test_inputs: pd.DataFrame, test_targets: pd.Series) -> float:
+        """Evaluate the regressor and return the r-squared score.
+
+        Args:
+            test_inputs (pandas.DataFrame): The features of the test data.
+            test_targets (pandas.Series): The targets of the test data.
+        Returns:
+            float: The r-squared score of the regressor.
+        """
+        if self.model is not None:
+            return self.model.score(test_inputs, test_targets)
+        raise NotFittedError("Model is not fitted yet.")
+
+    def save(self, model_dir_path: str) -> None:
+        """Save the regressor to disk.
+
+        Args:
+            model_dir_path (str): Dir path to which to save the model.
+        """
+        if not self._is_trained:
+            raise NotFittedError("Model is not fitted yet.")
+        joblib.dump(self, os.path.join(model_dir_path, PREDICTOR_FILE_NAME))
+
+    @classmethod
+    def load(cls, model_dir_path: str) -> "Regressor":
+        """Load the regressor from disk.
+
+        Args:
+            model_dir_path (str): Dir path to the saved model.
+        Returns:
+            Regressor: A new instance of the loaded regressor.
+        """
+        model = joblib.load(os.path.join(model_dir_path, PREDICTOR_FILE_NAME))
+        return model
+
+    def __str__(self):
+        # sort params alphabetically for unit test to run successfully
+        return (
+            f"Model name: {self.model_name} ("
+            f"activation: {self.activation}, "
+            f"hidden_layer_size: {self.hidden_layer_size}, "
+            f"learning_rate: {self.learning_rate}, "
+            f"solver: {self.solver})"
+        )
+
+
+def train_predictor_model(
+    train_inputs: pd.DataFrame, train_targets: pd.Series, hyperparameters: dict
+) -> Regressor:
+    """
+    Instantiate and train the predictor model.
+
+    Args:
+        train_X (pd.DataFrame): The training data inputs.
+        train_y (pd.Series): The training data targets.
+        hyperparameters (dict): Hyperparameters for the regressor.
+
+    Returns:
+        'Regressor': The regressor model
+    """
+    regressor = Regressor(**hyperparameters)
+    regressor.fit(train_inputs=train_inputs, train_targets=train_targets)
+    return regressor
+
+
+def predict_with_model(regressor: Regressor, data: pd.DataFrame) -> np.ndarray:
+    """
+    Predict regression targets for the given data.
+
+    Args:
+        regressor (Regressor): The regressor model.
+        data (pd.DataFrame): The input data.
+
+    Returns:
+        np.ndarray: The predicted regression targets.
+    """
+    return regressor.predict(data)
+
+
+def save_predictor_model(model: Regressor, predictor_dir_path: str) -> None:
+    """
+    Save the regressor model to disk.
+
+    Args:
+        model (Regressor): The regressor model to save.
+        predictor_dir_path (str): Dir path to which to save the model.
+    """
+    if not os.path.exists(predictor_dir_path):
+        os.makedirs(predictor_dir_path)
+    model.save(predictor_dir_path)
+
+
+def load_predictor_model(predictor_dir_path: str) -> Regressor:
+    """
+    Load the regressor model from disk.
+
+    Args:
+        predictor_dir_path (str): Dir path where model is saved.
+
+    Returns:
+        Regressor: A new instance of the loaded regressor model.
+    """
+    return Regressor.load(predictor_dir_path)
+
+
+def evaluate_predictor_model(
+    model: Regressor, x_test: pd.DataFrame, y_test: pd.Series
+) -> float:
+    """
+    Evaluate the regressor model and return the r-squared value.
+
+    Args:
+        model (Regressor): The regressor model.
+        x_test (pd.DataFrame): The features of the test data.
+        y_test (pd.Series): The targets of the test data.
+
+    Returns:
+        float: The r-sq value of the regressor model.
+    """
+    return model.evaluate(x_test, y_test)
